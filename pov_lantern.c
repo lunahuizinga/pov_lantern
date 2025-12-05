cb90b0d9172f2672cb96fd980b7bf332b3a2af04
Luna Huizinga <2197038+lunahuizinga@users.noreply.github.com>
2025-12-05 23:25:59 +0100

    Added SPI initialisation and abstraction functions

--- pov_lantern.c
+++ pov_lantern.c
@@ -13,6 +13,9 @@
 #define PIN_SCK  18
 #define PIN_COPI 19
 
+#define DOTSTAR_HEIGHT 8
+#define DOTSTAR_WIDTH 8
+
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
@@ -95,6 +98,25 @@ void set_pca9685_pin(uint8_t pin_num, bool on_state){
     set_pca9685_pwm(pin_num, 0, on_state ? 4095 : 0);
 }
 
+void write_to_spi(const uint8_t *src, size_t len){
+    spi_write_blocking(SPI_PORT, src, len);
+}
+
+uint32_t get_dotstar_pixel_data(uint8_t brightness, uint8_t red, uint8_t green, uint8_t blue){
+    return ((brightness | 0xE0) << 24) | (red << 16) | (green << 8) | blue;
+}
+
+void set_all_dotstar_pixels(uint8_t brightness, uint8_t red, uint8_t green, uint8_t blue){
+    uint8_t pixel_count = DOTSTAR_HEIGHT * DOTSTAR_WIDTH;
+    uint8_t pixel_data_length = pixel_count * 4;
+    uint8_t pixels_data[pixel_data_length];
+    uint32_t pixel_data = get_dotstar_pixel_data(brightness, red, green, blue);
+    for (size_t i = 0; i < pixel_data_length; i++){
+        pixels_data[i] = pixel_data << ((i + 1) % 4) * 8;
+    }
+    write_to_spi(pixels_data, pixel_data_length);
+}
+
 void setup_pca9685(){
     // Get the current mode register
     uint8_t current_mode = read_pca9685_reg(PCA9685_MODE1);
@@ -131,6 +153,7 @@ int main(){
     gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
     gpio_set_function(PIN_COPI, GPIO_FUNC_SPI);
+    spi_set_slave(SPI_PORT, false);
     
     // Chip select is active-low, so we'll initialise it to a driven-high state
     gpio_set_dir(PIN_CS, GPIO_OUT);
@@ -161,6 +184,8 @@ int main(){
     // H   L    CW
     set_pca9685_pin(TB6612FNG_IN1, clockwise);
     set_pca9685_pin(TB6612FNG_IN2, !clockwise);
+
+    set_all_dotstar_pixels(6, 0xFF, 0xFF, 0xFF);
 
     uint8_t index = 0;
     uint8_t registerOffset = 0x26;
